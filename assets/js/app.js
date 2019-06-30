134de33af29ee7c2f3105abe341f365057dc9689
Santiago Enciso <whereispolaris@gmail.com>
2019-06-30 14:06:27 -0500

    Planned application workflow with pseudocode.

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// PSEUDOCODE
+// =============================
+// When User Loads the page:
+// - 10 buttons in a certain category (e.g. animals) will display on the page. 
+// - When User Clicks on one of the button:
+//      * An API call will be made to Giphy API with the value of the button (e.g. skunk)
+//      * The response will generate 10 images STILL images to display on the page.
+// - When User Clicks on one of the still images:
+//      *  The image will animate.
+//      * If the image is clicked again, it will stop animating.
+// An input field will display to the page.
+// - When user enters a string to the field:
+//      * A button will generate and be added to the buttons (in an Array).
+// Bonus:
+//  - If page is refreshed, the added buttons will remain on the page.
