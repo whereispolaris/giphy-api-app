3674b2a124d7e71e9446a5a9df03dd129fb59cae
Santiago Enciso <whereispolaris@gmail.com>
2019-07-01 20:14:27 -0500

    Created even handler that plays/pauses the animation when the image is clicked.

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,10 +47,7 @@ function runQuery(queryURL) {
                 "data-status": "still",
 
                 "data-still": response.data[i].images.original_still.url,
-                "data-animate": response.data[i].images.original_still.url,
-
-                // data-still = still image
-                // data-animate = animated image
+                "data-animate": response.data[i].images.original.url,
             });
             var imageBody = $("<div>");
             imageBody.addClass("card-body");
@@ -87,29 +84,51 @@ $(document).on("click", ".topic-button", function (event) {
 
 });
 
+$(document).on("click", ".giphy", function (event) {
+    console.log("giphy clicked");
+    var giphy = $(this);
+    var status = giphy.data("status");
+    console.log(status);
+    console.log(giphy.data("animate"));
+    if (status === "still") {
+        console.log("The status is still");
+        giphy.attr({
+            src: giphy.data("animate"),
+            "data-status": "animate"
+        });
+    }
+    else if (status === "animate") {
+        console.log("The status is animate");
+        giphy.attr({
+            src: giphy.data("still"),
+            "data-status": "still"
+        });
+    }
+});
+
 renderButtons();
 
-                    // PSEUDOCODE
-                    // =============================
-                    // When User Loads the page:
-                    // - 10 buttons in a certain category (e.g. animals) will display on the page. 
-                    // - When User Clicks on one of the button:
-                    //      * An API call will be made to Giphy API with the value of the button (e.g. skunk)
-                    //      * The response will generate 10 images STILL images to display on the page along with the rating.
-                    // - When User Clicks on one of the still images:
-                    //      *  The image will animate.
-                    //      * If the image is clicked again, it will stop animating.
-                    // An input field will display to the page.
-                    // - When user enters a string to the field:
-                    //      * A button will generate and be added to the buttons (in an Array).
-                    // Bonus:
-                    //  - If page is refreshed, the added buttons will remain on the page.
+// PSEUDOCODE
+// =============================
+// When User Loads the page:
+// - 10 buttons in a certain category (e.g. animals) will display on the page. 
+// - When User Clicks on one of the button:
+//      * An API call will be made to Giphy API with the value of the button (e.g. skunk)
+//      * The response will generate 10 images STILL images to display on the page along with the rating.
+// - When User Clicks on one of the still images:
+//      *  The image will animate.
+//      * If the image is clicked again, it will stop animating.
+// An input field will display to the page.
+// - When user enters a string to the field:
+//      * A button will generate and be added to the buttons (in an Array).
+// Bonus:
+//  - If page is refreshed, the added buttons will remain on the page.
 
 
-                    // TO DO
-                    //==============
-                    // Create Div that contains each gif and:
-                    // - Add rating to image
-                    // - Add Still/Animate URLs as data
-                    // - Create contitions to animate/pause
+// TO DO
+//==============
+// Create Div that contains each gif and:
+// - Add rating to image - DONE
+// - Add Still/Animate URLs as data - DONE
+// - Create contitions to animate/pause
 // Add localStorage feature to the buttons.
