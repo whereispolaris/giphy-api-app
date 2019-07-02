21c745d66cab2d4f72bb92962dfde96547a76a76
Santiago Enciso <whereispolaris@gmail.com>
2019-07-01 19:45:03 -0500

    Added jQuery to generate more elements for the image component. This creates a bootstrap card and puts the image inside it. Also added more attributes to the image in order to animate/pause when user clicks.

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,15 +34,33 @@ function runQuery(queryURL) {
     }).then(function (response) {
         $("#gifDisplay").empty();
         for (var i = 0; i < numResults; i++) {
-            console.log(response.data[i].images.original.url);
-            console.log(response.data.length);
-            var image = $("<img>");
-            image.attr("src", response.data[i].images.original.url);
 
-            $("#gifDisplay").append(image);
-            // status = still/animate
-            // data-still = still image
-            // data-animate = animated image
+            // Generates Bootstrap Card 
+            var imageDiv = $("<div>");
+            imageDiv.addClass("card");
+            // Generates image
+            var image = $("<img>");
+            image.attr({
+                class: "giphy card-img-top",
+                src: response.data[i].images.original_still.url,
+                // status = still/animate
+                "data-status": "still",
+
+                "data-still": response.data[i].images.original_still.url,
+                "data-animate": response.data[i].images.original_still.url,
+
+                // data-still = still image
+                // data-animate = animated image
+            });
+            var imageBody = $("<div>");
+            imageBody.addClass("card-body");
+            var rating = $("<h5>");
+            rating.addClass("card-title");
+            rating.text("Rating: " + response.data[i].rating);
+            imageBody.append(rating);
+            imageDiv.append(image, imageBody);
+            $("#gifDisplay").append(imageDiv);
+
         }
     });
 }
@@ -53,7 +71,6 @@ function runQuery(queryURL) {
 // Event to Add more buttons to page
 $("#add-button").on("click", function () {
     var value = $("#topic-value").val().trim();
-    console.log(value);
     topics.push(value);
     renderButtons()
     return false;
@@ -65,7 +82,6 @@ $(document).on("click", ".topic-button", function (event) {
     event.preventDefault();
     topicSelected = $(this).attr("data-topic");
     var newURL = queryURL + "&q=" + topicSelected
-    console.log(newURL);
     newURL = newURL + "&limit" + numResults;
     runQuery(newURL);
 
@@ -73,27 +89,27 @@ $(document).on("click", ".topic-button", function (event) {
 
 renderButtons();
 
-// PSEUDOCODE
-// =============================
-// When User Loads the page:
-// - 10 buttons in a certain category (e.g. animals) will display on the page. 
-// - When User Clicks on one of the button:
-//      * An API call will be made to Giphy API with the value of the button (e.g. skunk)
-//      * The response will generate 10 images STILL images to display on the page along with the rating.
-// - When User Clicks on one of the still images:
-//      *  The image will animate.
-//      * If the image is clicked again, it will stop animating.
-// An input field will display to the page.
-// - When user enters a string to the field:
-//      * A button will generate and be added to the buttons (in an Array).
-// Bonus:
-//  - If page is refreshed, the added buttons will remain on the page.
+                    // PSEUDOCODE
+                    // =============================
+                    // When User Loads the page:
+                    // - 10 buttons in a certain category (e.g. animals) will display on the page. 
+                    // - When User Clicks on one of the button:
+                    //      * An API call will be made to Giphy API with the value of the button (e.g. skunk)
+                    //      * The response will generate 10 images STILL images to display on the page along with the rating.
+                    // - When User Clicks on one of the still images:
+                    //      *  The image will animate.
+                    //      * If the image is clicked again, it will stop animating.
+                    // An input field will display to the page.
+                    // - When user enters a string to the field:
+                    //      * A button will generate and be added to the buttons (in an Array).
+                    // Bonus:
+                    //  - If page is refreshed, the added buttons will remain on the page.
 
 
-// TO DO
-//==============
-// Create Div that contains each gif and:
-// - Add rating to image
-// - Add Still/Animate URLs as data
-// - Create contitions to animate/pause
+                    // TO DO
+                    //==============
+                    // Create Div that contains each gif and:
+                    // - Add rating to image
+                    // - Add Still/Animate URLs as data
+                    // - Create contitions to animate/pause
 // Add localStorage feature to the buttons.
